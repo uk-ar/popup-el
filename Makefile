dce792fd100bbf6214d4adc95f79c153c005debf
uk-ar <yuuki.ari@gmail.com>
2013-01-23 21:30:28 +0900

    Add ecukes environment

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 EMACS=emacs
+ECUKES = $(shell find elpa/ecukes-*/ecukes | tail -1)
 
 travis-ci:
 	${EMACS} --version
 	${EMACS} -batch -Q -l tests/run-test.el
+
+ecukes:
+	carton exec ${ECUKES} features
---
+++ features/support/env.el
@@ -0,0 +1,37 @@
+;; This is an example of how you could set up this file. This setup
+;; requires a directory called util in the project root and that the
+;; util directory contains the testing tools ert and espuds.
+
+(let* ((features-directory
+        (file-name-directory
+         (directory-file-name (file-name-directory load-file-name))))
+       (project-directory
+        (file-name-directory
+         (directory-file-name features-directory))))
+  (setq popup-el-root-path project-directory)
+  (setq popup-el-util-path (expand-file-name "util" popup-el-root-path)))
+
+(add-to-list 'load-path popup-el-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" popup-el-util-path))
+(add-to-list 'load-path (expand-file-name "ert" popup-el-util-path))
+
+(require 'popup)
+(require 'espuds)
+(require 'ert)
+
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
